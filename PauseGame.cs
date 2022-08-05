4e9e148e2d32dba3fa3289135ff38fc51d69f2c7
funcybear <92282784+funcybear@users.noreply.github.com>
2022-08-05 13:25:10 -0400

    PauseMenu 2D and 3D

---
+++ PauseGame.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseGame : MonoBehaviour
+{
+    public GameObject menu;
+    public GameObject resume;
+    public GameObject quit;
+
+    public bool on;
+    public bool off;
+
+
+
+
+
+    void Start()
+    {
+        menu.SetActive(false);
+        off = true;
+        on = false;
+    }
+
+
+
+
+    void Update()
+    {
+        if (off && Input.GetButtonDown("pause"))
+        {
+            Time.timeScale = 0;
+            menu.SetActive(true);
+            off = false;
+            on = true;
+        }
+
+        else if (on && Input.GetButtonDown("pause"))
+        {
+            Time.timeScale = 1;
+            menu.SetActive(false);
+            off = true;
+            on = false;
+        }
+        
+    }
+
+    public void Resume()
+    {
+            Time.timeScale = 1;
+            menu.SetActive(false);
+            off = true;
+            on = false;
+
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
